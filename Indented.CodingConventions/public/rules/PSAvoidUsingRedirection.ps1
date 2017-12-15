36a54581f7a7eb965cf2b87610ab606d560e9cba
Chris Dent <chris@indented.co.uk>
2017-12-15 15:48:48 +0000

    Added a basic rule to flag > and >> operators.

---
+++ Indented.CodingConventions/public/rules/PSAvoidUsingRedirection.ps1
@@ -0,0 +1,29 @@
+﻿using namespace Microsoft.Windows.PowerShell.ScriptAnalyzer.Generic
+using namespace System.Management.Automation.Language
+
+filter PSAvoidUsingRedirection {
+    <#
+    .SYNOPSIS
+        PSAvoidUsingRedirection
+    .DESCRIPTION
+        Avoid using redirection to write to files.
+    .EXAMPLE
+        Invoke-CodingConventionRule -ScriptBlock { 'string' > 'file.txt' } -RuleName PSAvoidUsingRedirection
+
+        Execute the rule against a script block using Invoke-CodingConventionRule.
+    #>
+
+    [CmdletBinding()]
+    [OutputType([Microsoft.Windows.PowerShell.ScriptAnalyzer.Generic.DiagnosticRecord])]
+    param (
+        [Parameter(ValueFromPipeline)]
+        [FileRedirectionAst]$ast
+    )
+
+    [DiagnosticRecord]@{
+        Message  = 'File redirection is being used to write file content in {0}.' -f $ast.Extent.File
+        Extent   = $ast.Extent
+        RuleName = $myinvocation.MyCommand.Name
+        Severity = 'Warning'
+    }
+}
